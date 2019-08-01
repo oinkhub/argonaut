48a985f46d1f8605c89bc5252ce61428f62b336c
vauxhall <zero.griffin@gmail.com>
2019-08-01 13:45:07 +0200

    Update plan

--- Source/Plan.swift
+++ Source/Plan.swift
@@ -1,13 +1,33 @@
 import Foundation
 
 public final class Plan {
+    public enum Mode: UInt8 {
+        case walking
+        case driving
+    }
+    
+    public struct Option {
+        public var mode = Mode.walking
+        public var duration = 0.0
+        public var distance = 0.0
+        public var points = [(Double, Double)]()
+    }
+    
+    public struct Path {
+        public var name = ""
+        public var latitude = 0.0
+        public var longitude = 0.0
+        public var options = [Option]()
+    }
+    
+    public var path = [Path]()
     public var route = [Route]()
     
     public init() { }
     
     func code() -> Data {
         var data = Data()
-        withUnsafeBytes(of: UInt32(route.count)) { data += $0 }
+//        withUnsafeBytes(of: UInt32(route.count)) { data += $0 }
         return Press().code(data)
     }
 }
