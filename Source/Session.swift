e981cedc5336f1d7965769c3a8a343ee1c2ac7ab
vauxhall <zero.griffin@gmail.com>
2019-08-08 07:40:26 +0200

    Update session

--- Source/Session.swift
+++ Source/Session.swift
@@ -1,1 +1,31 @@
+import Foundation
 
+public struct Session: Codable {
+    public struct Item: Codable {
+        public var name = ""
+        public var id = ""
+        public init() { }
+    }
+    
+    public static func load(_ result: @escaping((Session) -> Void)) {
+        DispatchQueue.global(qos: .background).async {
+            let session = {
+                $0 == nil ? Session() : (try? JSONDecoder().decode(Session.self, from: $0!)) ?? Session()
+            } (UserDefaults.standard.data(forKey: "session"))
+            DispatchQueue.main.async { result(session) }
+        }
+    }
+    
+    public var items = [Item]() { didSet { save() } }
+    public var rating = Calendar.current.date(byAdding: {
+        var d = DateComponents()
+        d.day = 3
+        return d
+    } (), to: Date())! { didSet { save() } }
+    
+    private func save() {
+        DispatchQueue.global(qos: .background).async {
+            UserDefaults.standard.set(try! JSONEncoder().encode(self), forKey: "session")
+        }
+    }
+}
--- Tests/TestSession.swift
+++ Tests/TestSession.swift
@@ -1,1 +1,59 @@
+@testable import Argonaut
+import XCTest
 
+final class TestSession: XCTestCase {
+    override func setUp() {
+        UserDefaults.standard.removeObject(forKey: "session")
+    }
+    
+    func testLoad() {
+        let expect = expectation(description: "")
+        let dateMin = Calendar.current.date(byAdding: {
+            var d = DateComponents()
+            d.day = 3
+            return d
+        } (), to: Date())!
+        let dateMax = Calendar.current.date(byAdding: {
+            var d = DateComponents()
+            d.day = 4
+            return d
+        } (), to: Date())!
+        DispatchQueue.global(qos: .background).async {
+            Session.load {
+                XCTAssertGreaterThanOrEqual($0.rating, dateMin)
+                XCTAssertLessThan($0.rating, dateMax)
+                XCTAssertEqual(.main, Thread.current)
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testRating() {
+        let expect = expectation(description: "")
+        let date = Date()
+        var session = Session()
+        session.rating = date
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.01) {
+            Session.load {
+                XCTAssertEqual(date, $0.rating)
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testItems() {
+        let expect = expectation(description: "")
+        var session = Session()
+        session.items = [.init()]
+        session.items[0].id = "hello"
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.01) {
+            Session.load {
+                XCTAssertEqual("hello", $0.items[0].id)
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+}
