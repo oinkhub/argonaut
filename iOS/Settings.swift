c1cfdeb26fe6075239b6f78e0d776e4427de527b
vauxhall <zero.griffin@gmail.com>
2019-09-04 23:56:18 +0100

    Update settings

--- iOS/Settings.swift
+++ iOS/Settings.swift
@@ -31,51 +31,18 @@ final class Settings: UIView {
         let base = UIView()
         base.translatesAutoresizingMaskIntoConstraints = false
         base.backgroundColor = .black
-        base.layer.cornerRadius = 4
+        base.layer.cornerRadius = 6
         addSubview(base)
-        
-        let title = UILabel()
-        title.translatesAutoresizingMaskIntoConstraints = false
-        title.font = .systemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize, weight: .medium)
-        title.textColor = .halo
-        title.isAccessibilityElement = true
-        title.accessibilityTraits = .staticText
-        title.text = .key("Settings.title")
-        base.addSubview(title)
-        
-        let close = UIButton()
-        close.translatesAutoresizingMaskIntoConstraints = false
-        close.isAccessibilityElement = true
-        close.accessibilityLabel = .key("Settings.close")
-        close.setImage(UIImage(named: "close"), for: .normal)
-        close.imageView!.clipsToBounds = true
-        close.imageView!.contentMode = .center
-        close.addTarget(self, action: #selector(self.close), for: .touchUpInside)
-        base.addSubview(close)
         
         base.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
         base.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
-        base.heightAnchor.constraint(equalToConstant: 450).isActive = true
-        top = base.topAnchor.constraint(equalTo: topAnchor, constant: -460)
+        base.heightAnchor.constraint(equalToConstant: 480).isActive = true
+        top = base.topAnchor.constraint(equalTo: topAnchor, constant: -490)
         top.isActive = true
-        
-        title.leftAnchor.constraint(equalTo: base.leftAnchor, constant: 20).isActive = true
-        title.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        close.centerYAnchor.constraint(equalTo: title.centerYAnchor).isActive = true
-        close.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        close.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        close.rightAnchor.constraint(equalTo: base.rightAnchor).isActive = true
-        
-        if #available(iOS 11.0, *) {
-            title.topAnchor.constraint(equalTo: base.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-        } else {
-            title.topAnchor.constraint(equalTo: base.topAnchor, constant: 20).isActive = true
-        }
     }
     
     @objc private func close() {
-        top.constant = -460
+        top.constant = -490
         UIView.animate(withDuration: 0.4, animations: { [weak self] in
             app.view.layoutIfNeeded()
             self?.alpha = 0
