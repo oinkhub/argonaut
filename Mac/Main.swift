85e34aa3e36023745ef4d41d32c2ce7da2694bc1
vauxhall <zero.griffin@gmail.com>
2019-09-24 08:52:26 +0200

    Update drag

--- Mac/Main.swift
+++ Mac/Main.swift
@@ -1,12 +1,43 @@
 import AppKit
 
 final class Main: Window {
+    private class Dragger: NSView {
+        required init?(coder: NSCoder) { nil }
+        init() {
+            super.init(frame: .zero)
+            translatesAutoresizingMaskIntoConstraints = false
+            if #available(OSX 10.13, *) {
+                registerForDraggedTypes([.fileURL])
+            } else {
+                registerForDraggedTypes([NSPasteboard.PasteboardType(kUTTypeFileURL as String)])
+            }
+        }
+        
+        override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+            NSCursor.dragCopy.set()
+            return super.draggingEntered(sender)
+        }
+        
+        override func draggingExited(_: NSDraggingInfo?) { NSCursor.arrow.set() }
+        
+        override func draggingEnded(_ sender: NSDraggingInfo) {
+            if frame.contains(sender.draggingLocation) {
+                if let url = (sender.draggingPasteboard.propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? NSArray)?[0] as? String {
+                    app.receive(URL(fileURLWithPath: url))
+                }
+            }
+        }
+    }
+    
     private(set) weak var bar: Bar!
     private(set) weak var base: NSView!
 
     init() {
         super.init(800, 600, mask: [.miniaturizable, .resizable])
         minSize = .init(width: 400, height: 200)
+        
+        let dragger = Dragger()
+        contentView!.addSubview(dragger)
         
         let bar = Bar()
         contentView!.addSubview(bar)
@@ -18,6 +49,11 @@ final class Main: Window {
         base.layer!.backgroundColor = .ui
         contentView!.addSubview(base)
         self.base = base
+        
+        dragger.topAnchor.constraint(equalTo: contentView!.topAnchor).isActive = true
+        dragger.bottomAnchor.constraint(equalTo: contentView!.bottomAnchor).isActive = true
+        dragger.leftAnchor.constraint(equalTo: contentView!.leftAnchor).isActive = true
+        dragger.rightAnchor.constraint(equalTo: contentView!.rightAnchor).isActive = true
         
         bar.topAnchor.constraint(equalTo: contentView!.topAnchor).isActive = true
         bar.bottomAnchor.constraint(equalTo: contentView!.bottomAnchor).isActive = true
